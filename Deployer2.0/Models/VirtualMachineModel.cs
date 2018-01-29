90f9f10531a97960f7bfb8699350c363381b2753
Binah Smith <bsmith@student.neumont.edu>
2018-01-29 10:44:36 -0700

    Added new bootstrap configuration. With new controllers, and view objects.

--- Deployer2.0/Models/VirtualMachineModel.cs
+++ Deployer2.0/Models/VirtualMachineModel.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Drawing;
+using System.IO;
 
 namespace Deployer2._0.Models
 {
     public class VirtualMachineModel
     {
+        
         public string Name { get; set; }
         public string Description { get; set; }
         public string IPAddress { get; set; }
         public string OperatingSystem { get; set; }
         public int Memory { get; set; }
         public int CPUs { get; set; }
+
 
         public override string ToString()
         {
