c3094d1dddc1587ba0c2a9b4a5fa7cc78d6915d2
smithbinah <bsmith@student.neumont.edu>
2018-01-29 19:07:16 -0700

    adding untracked files

---
+++ Deployer2.0/Controllers/ContainerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Deployer2._0.Models;
+
+namespace Deployer2._0.Controllers
+{
+    public class ContainerController : Controller
+    {
+        // GET: Container
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult CreationForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult CreationForm(ContainerModel creationResponse)
+        {
+
+            return View("Thanks",creationResponse);
+        }
+    }
+}
---
+++ Deployer2.0/Models/ContainerModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Deployer2._0.Models
+{
+    public class ContainerModel
+    {
+        public string IPAddress { get; set; }
+        public string Name { get; set; }
+    }
+}
